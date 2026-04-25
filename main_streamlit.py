74614a708e416e0f56af5e67fde4b1c8aa5039eb
Gabriel Quinche <gabrielquinche@hotmail.com>
2026-04-25 07:51:12 -0500

    Update main_streamlit.py
    remove redundant title and reduce size of text of calendar

--- main_streamlit.py
+++ main_streamlit.py
@@ -177,7 +177,6 @@ def get_inferred_country(client_ip=None):
     return "Colombia"
 
 def main(country="Colombia", year=date.today().year):
-    st.title(f"Holidays in {country}")
 
     # Create a holidays object for the selected country
     country_to_iso = {c.name: c.alpha_2 for c in pycountry.countries}
@@ -196,7 +195,7 @@ def main(country="Colombia", year=date.today().year):
 
 
     # Display the holidays in a calendar view (First thing to show for the current year)
-    st.write(f"### Calendar of Holidays in {country} ({year})")
+    st.write(f"#### Holidays in {country} ({year})")
     calendar_events = [
         {"title": name, "start": d.isoformat(), "allDay": True}
         for d, name in country_holidays.items()
